3a9e9699741f37993f262ee6567a737772c38afa
PaulHuaman696 <112147264+PaulHuaman696@users.noreply.github.com>
2025-05-13 12:50:53 -0500

    Update firebaseConfig.ts

--- frontend/src/firebase/firebaseConfig.ts
+++ frontend/src/firebase/firebaseConfig.ts
@@ -1,17 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDqPHOOgumHk25nUE4fp9QmxC-nQudBDKA",
-  authDomain: "splity-1a02e.firebaseapp.com",
-  projectId: "splity-1a02e",
-  storageBucket: "splity-1a02e.firebasestorage.app",
-  messagingSenderId: "432131681494",
-  appId: "1:432131681494:web:8f087926a256a5ac49ad77",
-  measurementId: "G-RXCWW3NHVV"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // 👈 Usamos Firebase Auth, no analytics
-
-export { auth };
