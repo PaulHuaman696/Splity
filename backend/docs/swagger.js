95655920d103d551b6a9b09a60ef1ced156ad9c6
PaulHuaman696 <anderson.huaman@unas.edu.pe>
2025-07-23 10:32:11 -0500

    add documentation with swagger

--- backend/docs/swagger.js
+++ backend/docs/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000/api', // Ajusta al entorno de despliegue
+        url: 'https://api-movigo.space/api', // Ajusta al entorno de despliegue
       },
     ],
     components: {
